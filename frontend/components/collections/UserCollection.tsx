253bd021fa1bd9d8888705e0576d0eefd8ccabbd
JonFactor <Jon.Factor2@gmail.com>
2024-01-04 18:39:12 -0500

    forrgot to add

---
+++ frontend/components/collections/UserCollection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const UserCollection = () => {
+  return (
+    <View>
+      <Text>UserCollection</Text>
+    </View>
+  );
+};
+
+export default UserCollection;
