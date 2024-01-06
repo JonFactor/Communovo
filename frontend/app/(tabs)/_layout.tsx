294e5ecc8a34ebe33e0d153574d0ca98a2c4f348
JonFactor <Jon.Factor2@gmail.com>
2024-01-06 17:48:09 -0500

    navbar icons finsihed

--- frontend/app/(tabs)/_layout.tsx
+++ frontend/app/(tabs)/_layout.tsx
@@ -1,51 +1,97 @@
 import { Image } from "expo-image";
 import { Tabs } from "expo-router";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { useAssets } from "expo-asset";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import ProfilePictureCard from "../../components/cards/ProfilePictureCard";
 import * as calendar from "expo-calendar";
+import { Tab } from "react-native-elements";
 export default () => {
-  const logoSetting = (imgSrc, focused) => {
-    if (focused) {
-      if (imgSrc.toString().includes("home")) {
-        console.log(imgSrc);
+  const logoSetting = (imgSrc: string, focused) => {
+    let imageSource;
+    if (imgSrc.includes("house")) {
+      if (focused) {
+        imageSource = require("../../assets/navbarIcons/houseFilled.svg");
+      } else {
+        imageSource = require("../../assets/navbarIcons/houseUnfilled.svg");
+      }
+    } else if (imgSrc.includes("groups")) {
+      if (focused) {
+        imageSource = require("../../assets/navbarIcons/discoverFilled.svg");
+      } else {
+        imageSource = require("../../assets/navbarIcons/discoverUnfilled.svg");
+      }
+    } else if (imgSrc.includes("profile")) {
+      if (focused) {
+        imageSource = require("../../assets/navbarIcons/profileFilled.svg");
+      } else {
+        imageSource = require("../../assets/navbarIcons/profileUnfilled.svg");
       }
     }
-
+    // const imageSource = require(imgSrc.toString());
     // if (imgSrc !== "") {
     //   imgSrc = require(imgSrc);
     // }
     return (
-      <View style={styles.logoContain}>
-        {imgSrc !== "" && (
-          <Image style={styles.logo} source={imgSrc} contentFit="cover" />
-        )}
+      <View style={styles.mainContain}>
+        {/* <View style={styles.logoAbove}></View> */}
+        <View style={styles.logoContain}>
+          {imgSrc !== "" && (
+            <Image style={styles.logo} source={imageSource} contentFit="fill" />
+          )}
+        </View>
+      </View>
+    );
+  };
+
+  const HomeScreen = () => {
+    return (
+      <View>
+        <Text>Hello</Text>
       </View>
     );
   };
 
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{ headerShown: false }}
+      sceneContainerStyle={styles.wholeContain}
+    >
       <Tabs.Screen
         name="home"
         options={{
           tabBarIcon: (params) =>
             logoSetting(
-              require("../../assets/navbarIcons/house.png"),
+              "../../assets/navbarIcons/house.png",
               params["focused"]
             ),
+          tabBarStyle: {
+            height: 90,
+            paddingVertical: 4,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
         }}
-      ></Tabs.Screen>
+
+        // compon
+      />
       <Tabs.Screen
         name="discover"
         options={{
           tabBarIcon: (params) =>
             logoSetting(
-              require("../../assets/navbarIcons/groups.png"),
+              "../../assets/navbarIcons/groups.png",
               params["focused"]
             ),
+          tabBarStyle: {
+            height: 90,
+            paddingVertical: 4,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
         }}
       />
       <Tabs.Screen
@@ -53,9 +99,16 @@ export default () => {
         options={{
           tabBarIcon: (params) =>
             logoSetting(
-              require("../../assets/navbarIcons/profile.png"),
+              "../../assets/navbarIcons/profile.png",
               params["focused"]
             ),
+          tabBarStyle: {
+            height: 90,
+            paddingVertical: 4,
+          },
+          tabBarLabelStyle: {
+            fontSize: 12,
+          },
         }}
       />
       <Tabs.Screen
@@ -92,13 +145,25 @@ export default () => {
 
 const styles = StyleSheet.create({
   logoContain: {
-    width: 50,
+    width: 90,
     aspectRatio: 1,
     flex: 1,
   },
   logo: {
     flex: 1,
     width: "100%",
-    borderRadius: 40,
+  },
+  logoAbove: {
+    width: 42,
+    height: 8,
+    backgroundColor: "#C8B6FF",
+    borderRadius: 90,
+  },
+  mainContain: {
+    flex: 1,
+    marginTop: 2,
+  },
+  wholeContain: {
+    flex: 1,
   },
 });
