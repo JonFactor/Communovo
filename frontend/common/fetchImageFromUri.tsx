4655e5a9e267b43ecfddced6dee8c704b0a82d85
JonFactor <Jon.Factor2@gmail.com>
2024-01-14 22:06:59 -0500

    done

---
+++ frontend/common/fetchImageFromUri.tsx
@@ -0,0 +1,6 @@
+export const fetchImageFromUri = async (uri: string) => {
+  const response = await fetch(uri);
+  const blob = await response.blob();
+
+  return blob;
+};
---
+++ frontend/components/modals/EventOrGroupCreationModal.tsx
@@ -0,0 +1,64 @@
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { Linker } from "../../utils/Linker";
+
+interface IAppInfoModalParams {
+  parentSetter: (param) => void;
+}
+
+const EventOrGroupCreationModal = ({ parentSetter }: IAppInfoModalParams) => {
+  const handleEventClick = () => {
+    const redirected = Linker("/events");
+    if (!redirected) {
+      parentSetter(true);
+      return;
+    }
+    parentSetter(false);
+  };
+
+  const handleGroupClick = () => {
+    const redirected = Linker("/createGroup");
+    if (!redirected) {
+      parentSetter(true);
+      return;
+    }
+    parentSetter(false);
+  };
+
+  return (
+    <View className=" w-full flex items-center mt-16">
+      <View className=" w-72 flex items-center flex-col space-y-12 mt-12">
+        <TouchableOpacity
+          className=" w-64 h-72 bg-md-blue p-12 rounded-3xl"
+          onPress={handleGroupClick}
+        >
+          <View className=" flex w-full items-center space-y-8">
+            <Text className=" text-3xl font-semibold justify-center text-center">
+              Start a new Group!
+            </Text>
+            <Text className=" text-5xl">📢😄</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className=" w-64 h-72  bg-md-purple p-4 rounded-3xl"
+          onPress={handleEventClick}
+        >
+          <View className=" flex w-full items-center space-y-8 mt-12">
+            <Text className=" text-3xl font-semibold text-center justify-center">
+              Start a new Event!
+            </Text>
+            <Text className=" text-4xl">🥳🥳🥳</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className=" w-64 h-24 rounded-2xl bg-red-200 items-center"
+          onPress={() => parentSetter(false)}
+        >
+          <Text className=" mt-7 font-semibold text-3xl">Give up {":("}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default EventOrGroupCreationModal;
